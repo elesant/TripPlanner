f2a2d6576c88d20d0d0fc80b4c2b942c644d2efc
Sean Li <lishang106@gmail.com>
2013-05-18 16:50:01 -0400

    Added raven configuration.

--- grouptrotter/settings_prod.py
+++ grouptrotter/settings_prod.py
@@ -30,3 +30,8 @@ FACEBOOK_APP_SECRET = 'ac86a28d1b4209937fee5b16ed89baa7'
 # AWS Information
 AWS_ACCESS_KEY_ID = 'AKIAIWUXQY7UILQNXKDA'
 AWS_SECRET_ACCESS_KEY = 'Mf/DGgaXrePLNS4M3bmBqLv9shUz/xH71PqqI4s9'
+
+# Set your DSN value
+RAVEN_CONFIG = {
+    'dsn': 'https://6c5c2f8ec9cd41abaf43c4179c49a7b7:b2c753b00ef44469a0e916d6bf3272c6@app.getsentry.com/8596',
+}
