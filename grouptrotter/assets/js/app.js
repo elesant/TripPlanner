6a68c41d76ded1be306d905038a9c393dca68aae
Jeffrey Morgan <jmorganca@gmail.com>
2013-05-18 15:04:04 -0400

    working drag drop, basic dashboard down

--- grouptrotter/assets/js/app.js
+++ grouptrotter/assets/js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('app', ['firebase']);
+
+$(document).ready(function () {
+  var offsetFn = function() {
+    return $('#affix-sidebar').position().top;
+  };
+  $('#affix-sidebar').affix({
+    offset: {top: offsetFn}
+  });
+
+  $('.navbar-fixed-top').affix();
+});
